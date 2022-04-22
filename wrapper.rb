3e21e45c2cfedeef41bc7b1df9b175567ef63978
Leslie Kornes <lesliekornes@gmail.com>
2022-04-22 09:47:27 -0400

    add comment of a bit DRYer version with additional method call

--- wrapper.rb
+++ wrapper.rb
@@ -23,7 +23,6 @@ class Wrapper
       if (rows.last.length + s.length) < width
         x = rows[-1]
         rows[-1] = "#{x} " + "#{s}"
-      # _really_ hate having to repeat the above code here XO
       elsif s.length > width
         string_length_counter = s.length
         while string_length_counter > width
@@ -40,4 +39,43 @@ class Wrapper
   end
 end
 
+# ** this is DRY'd up a bit. the reqs said only to have one `wrap` method so i had to repeat above **
+# class Wrapper
+#   def self.wrap(string, width)
+#     return string if string.length <= width
 
+#     incoming_strings = string.split
+
+#     rows = []
+
+#     first_str = incoming_strings.first
+
+#     if first_str.length > width
+#       split_longer_words(first_str, width, rows)
+#     else
+#       rows << incoming_strings.shift
+#     end
+
+#     incoming_strings.each do |s|
+#       if (rows.last.length + s.length) < width
+#         x = rows[-1]
+#         rows[-1] = "#{x} " + "#{s}"
+#       elsif s.length > width
+#         split_longer_words(s, width, rows)
+#         rows << s
+#       else
+#         rows << s
+#       end
+#     end
+#     puts rows
+#   end
+
+#   def self.split_longer_words(string, width, rows_array)
+#     string_length_counter = string.length
+#       while string_length_counter >= width
+#         rows_array << string[0...width]
+#         string.delete_prefix!(string[0...width])
+#         string_length_counter -= width
+#       end
+#   end
+# end
